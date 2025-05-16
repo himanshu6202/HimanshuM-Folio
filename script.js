df7361617091df9490983a59d4cd0ecbdf2d3991
himanshu6202 <nomadhimanshu6202@gmail.com>
2025-05-16 18:47:41 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,54 @@
+var tl = gsap.timeline()
+
+tl.from('#nav-left', {
+    y: -30,
+    opacity: 0,
+    delay: 0.5,
+    duration: 0.5,
+})
+tl.from('#menu', {
+    opacity: 0,
+    duration: 0.5,
+})
+tl.from('#nav-center h2', {
+    y: -30,
+    opacity: 0,
+    delay: 0.5,
+    duration: 0.5,
+    stagger: 0.4
+})
+tl.from('#nav-right', {
+    opacity: 0,
+    duration: 0.5,
+})
+tl.from('#profile-photo', {
+    height: 0,
+    duration: 1,
+})
+tl.from('#hero-section h1', {
+    opacity: 0,
+    border: 1,
+    delay: 0.5,
+    duration: 0.5,
+    stagger: 0.4
+})
+tl.from('#hero-section h2', {
+    opacity: 0,
+    duration: 2,
+    ease: "slow(0.7,0.7,true)"
+})
+tl.from('#hero-bottom', {
+    opacity: 0,
+    width: 0,
+    duration: 2,
+    ease: "slow(0.7,0.7,true)"
+})
+
+
+
+
+
+// gsap.from('.service-video', {
+//     scale : 0,
+//     delay: 1,
+// })
