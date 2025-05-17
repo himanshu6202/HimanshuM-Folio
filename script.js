f422f8044f135fa7ee35343cd2a8ba53c557bc71
himanshu6202 <nomadhimanshu6202@gmail.com>
2025-05-17 19:48:15 +0530

    desgin done before footer

--- script.js
+++ script.js
@@ -3,19 +3,19 @@ var tl = gsap.timeline()
 tl.from('#nav-left', {
     y: -30,
     opacity: 0,
-    delay: 0.5,
+    delay: 0.2,
     duration: 0.5,
 })
 tl.from('#menu', {
     opacity: 0,
-    duration: 0.5,
+    duration: 0.2,
 })
 tl.from('#nav-center h2', {
     y: -30,
     opacity: 0,
     delay: 0.5,
     duration: 0.5,
-    stagger: 0.4
+    stagger: 0.3
 })
 tl.from('#nav-right', {
     opacity: 0,
@@ -39,9 +39,9 @@ tl.from('#hero-section h2', {
 })
 tl.from('#hero-bottom', {
     opacity: 0,
-    width: 0,
-    duration: 2,
-    ease: "slow(0.7,0.7,true)"
+    x: 12,
+    duration: 1,
+    // ease: "slow(0.7,0.7,true)"
 })
 
 
